dec26e94ac9d4b84e942ed5cac361b461548ea5e
Deborah Chrotich <cherotichdeborahb@gmail.com>
2024-04-13 21:34:02 +0300

    add file

---
+++ tuple.py
@@ -0,0 +1,20 @@
+# accessing item
+fruits =("Banana","Pineaple","Cherry","Mango","Kiwi","Avocado")
+print((fruits[1:5]))
+print(fruits[-1])
+
+# Adding items
+fruit2 =[fruits]
+print((fruit2))
+fruit2.append("Lemon")
+print(fruit2)
+
+# Removing items
+print(fruit2.pop())
+
+
+
+
+
+
+
